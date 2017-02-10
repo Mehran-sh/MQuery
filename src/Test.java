c52fbf6e0c39857647c184d1688857460db9c908
Mehran <MehranMsh@yahoo.com>
2017-02-10 09:40:57 +0100

    added orderby test

--- src/Test.java
+++ src/Test.java
@@ -11,12 +11,12 @@ public class Test {
 	public Flow<Book> getQuery()
 	{
 		ArrayList<Book> d = new ArrayList<>();
-		d.add(new Book(1, "Book 1"));
+		d.add(new Book(6, "Book 6"));
 		d.add(new Book(4, "Book 4"));
 		d.add(new Book(5, "Book 5"));
 		d.add(new Book(2, "Book 2"));
 		d.add(new Book(3, "Book 3"));
-		d.add(new Book(6, "Book 6"));
+		d.add(new Book(1, "Book 1"));
 		return Flow.query(d);
 	}
 	
@@ -26,6 +26,16 @@ public class Test {
 		Book result = f.where(v -> v.id == 2).firstOrDefault();
 		Assert.assertNotNull(result);
 		Assert.assertEquals(result.id, 2);
+	}
+	
+	@org.junit.Test
+	public void orderByTest()
+	{
+		Flow<Book> f = getQuery();
+		f = f.orderBy(b -> b.id);
+		ArrayList<Book> result = f.all();
+		assertEquals(result.get(0).id, 1);
+		assertEquals(result.get(5).id, 6);
 	}
 
 }
